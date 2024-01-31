e5f7ee09f3ce9766e72ddae6818a44a4ee54e648
Jip Fr <jipfrijlink@gmail.com>
2024-01-31 02:12:23 +0100

    Count

---
+++ storage/count.lua
@@ -0,0 +1,67 @@
+local args = {...}
+
+-- Args to 1 string
+args = table.concat(args, ' ')
+
+-- Import utils file
+local utilsFile = fs.open('utils.lua', 'r')
+local utils = utilsFile.readAll()
+utilsFile.close()
+loadstring(utils)()
+
+local chests = getChests()
+
+-- ! Find items in chest
+-- Create variable called "query" that removes all numbers
+local query = string.gsub(args, '%d+', '')
+local itemToFind = toItemName(query)
+
+print(toLeft('Searching'), itemToFind)
+
+local foundCount = 0
+
+-- Loop through chests
+for i = 1, #chests do
+    -- Get chest
+    local chest = chests[i]
+    if chest == nil then
+        break
+    end
+
+    -- Get list of items
+    local items = chest.list()
+    if items == nil then
+        print('No items found')
+        break
+    end
+
+    -- Loop through table of items
+    for i = 1, 54 do
+        -- Get item
+        local item = items[i]
+        if item ~= nil then
+
+            -- Take name, split by : and get last item
+            local name = toItemName(item.name)
+
+            -- Check if item is the one we are looking for
+            if name == itemToFind then
+                -- Find number to take out of chest
+
+                foundCount = foundCount + item.count
+
+                print(toLeft(itemToFind), 'Found ' .. foundCount)
+
+            end
+        end
+    end
+end
+
+local w, h = term.getSize()
+print(toLeft(''), string.rep('-', (w - 1) - #toLeft('')))
+
+-- print(foundCount .. ' ' .. itemToFind .. ' found (' .. math.floor((foundCount / 64) + 0.5, 2) .. ' stacks)')
+print(toLeft('Found'), foundCount .. ' ' .. itemToFind)
+print(toLeft(''), math.floor((foundCount / 64) + 0.5, 2) .. ' stacks')
+print(toLeft(''), math.floor((foundCount / (64 * 27)) + 0.5, 2) .. ' double chests')
+
--- storage/update.lua
+++ storage/update.lua
@@ -4,32 +4,29 @@ local urls = {
     ['update'] = baseUrl .. '/update.lua',
     ['get'] = baseUrl .. '/get.lua',
     ['deposit'] = baseUrl .. '/deposit.lua',
-    ['utils'] = baseUrl .. '/utils.lua'
+    ['utils'] = baseUrl .. '/utils.lua',
+    ['count'] = baseUrl .. '/count.lua'
 }
 
 -- Loop over urls
 for key, url in pairs(urls) do
     -- Get response
     local response = http.get(url)
-    if response == nil then
+
+    if response ~= nil then
+        -- Read response
+        local responseText = response.readAll()
+
+        -- Write to file
+        local file = fs.open(key .. '.lua', 'w')
+        file.write(responseText)
+        file.close()
+        print('Saved ' .. key .. '.lua')
+
+        -- Close response
+        response.close()
+    else
         print('Failed to get response from ' .. url)
-        return
     end
 
-    -- Read response
-    local responseText = response.readAll()
-
-    if responseText == nil then
-        print('Failed to read response from ' .. url)
-        return
-    end
-
-    -- Write to file
-    local file = fs.open(key .. '.lua', 'w')
-    file.write(responseText)
-    file.close()
-    print('Saved ' .. key .. '.lua')
-
-    -- Close response
-    response.close()
 end
