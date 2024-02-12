db252ecf1abd74c86d0d63277f56396e4c934d04
Sting Alleman <stingalleman@icloud.com>
2024-02-12 03:21:15 +0100

    test

--- storage/test.lua
+++ storage/test.lua
@@ -1,22 +1,22 @@
 -- ! Find all chests and barrels
 local allPeripherals = peripheral.getNames()
-local chests = {}
-for i = 1, #allPeripherals do
-    local peripheralName = allPeripherals[i]
-    if peripheral.getType(peripheralName) == 'minecraft:chest' or
-        (includeTrappedChests and peripheral.getType(peripheralName) == 'minecraft:trapped_chest') then
-        local c = peripheral.wrap(peripheralName)
+-- local chests = {}
+-- for i = 1, #allPeripherals do
+--     local peripheralName = allPeripherals[i]
+--     if peripheral.getType(peripheralName) == 'minecraft:chest' or
+--         (includeTrappedChests and peripheral.getType(peripheralName) == 'minecraft:trapped_chest') then
+--         local c = peripheral.wrap(peripheralName)
 
-        -- Add name to c
-        c.name = peripheralName
-        local s = split(peripheralName, '_')
-        c.id = s[#s]
-        if peripheral.getType(peripheralName) == 'minecraft:trapped_chest' then
-            c.id = 'T' .. c.id
-        end
+--         -- Add name to c
+--         c.name = peripheralName
+--         local s = split(peripheralName, '_')
+--         c.id = s[#s]
+--         if peripheral.getType(peripheralName) == 'minecraft:trapped_chest' then
+--             c.id = 'T' .. c.id
+--         end
 
-        table.insert(chests, c)
-    end
-end
+--         table.insert(chests, c)
+--     end
+-- end
 
-print(toLeft('System'), 'Found ' .. #chests .. ' chests')
+-- print(toLeft('System'), 'Found ' .. #chests .. ' chests')
